6faa3a70935d9d53093e405ecbe9b7c4ba955eb3
sam8e1-777 <samvelsymonian@gmail.com>
2025-10-13 17:16:32 +0200

    Initial commit

--- api/index.js
+++ api/index.js
@@ -4,16 +4,26 @@ const path = require("path");
 
 // EJS template engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views")); // note ../ because api/ is one level deep
 
 // static files from public/
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
+// Routes
 app.get("/", (req, res) => {
-  res.render("layout", { title: "Samvel`s Portfolio", body: "pages/index", page:"home"});
+  res.render("layout", { 
+    title: "Samvel's Portfolio", 
+    body: "pages/index", 
+    page: "home" 
+  });
 });
+
 app.get("/projects", (req, res) => {
-  res.render("layout", { title: "My Projects", body: "pages/projects", page: "projects"});
+  res.render("layout", { 
+    title: "My Projects", 
+    body: "pages/projects", 
+    page: "projects" 
+  });
 });
 
 module.exports = app;
